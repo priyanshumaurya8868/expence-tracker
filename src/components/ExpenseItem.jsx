ad9a08536e660e91f6c3de51b68dc2f3e5c44eec
priyanshumaurya8868 <priyanshumaurya.8868@gmail.com>
2023-01-20 13:10:38 +0530

    02_multiple_components

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,9 +1,10 @@
 import "./ExpenseItem.css"
+import ExpenseDate from "./ExpenseDate"
  
  const ExpenseItem = (prop) => {
   return (
     <div className="expense-item">
-      <div>{prop.date.toISOString()}</div>
+    <ExpenseDate date={prop.date} />
       <div className="expense-item__description">
         <h2>{prop.title}</h2>
         <div className="expense-item__price">{prop.amount}</div>
