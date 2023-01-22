a7449ee658ee2f122917d9bae89f1287065bb571
priyanshumaurya8868 <priyanshumaurya.8868@gmail.com>
2023-01-22 23:47:26 +0530

    fix: typo

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Card.css"
 const Card = (props) => {
-    const className = "card "+props.classNameyyy;
+    const className = "card "+props.className;
   return (
     <div className={className}>{props.children}</div>
   )
